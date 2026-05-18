7d64ecba9c9c67ce97b7492d75500b4b50469a20
Snarpxx <vfrancelaurence@gmail.com>
2026-05-18 23:42:09 +0800

    changed title

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
 
 export const metadata: Metadata = {
-  title: 'MyQR - Advanced QR Generator',
+  title: 'MyQR - QR Code Generator',
   description: 'Create, customize, preview, and download highly personalized QR codes for virtually anything. Free and Open Source.',
 };
 
